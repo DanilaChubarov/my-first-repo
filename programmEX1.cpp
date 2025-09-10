47afee5d8d8bd3018037de6965a73614541d01e8
DanilaChubarov <thubarov21@gmail.com>
2025-09-10 12:33:42 +0300

    sas

---
+++ programmEX1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+
+int main() {
+	int a, b, d;
+	std::cout << "Enter a,b,d" << std::endl;
+	std::cin >> a >> b >> d;
+	for (int i = 1; i < 5; i+1);
+	{ std::cout << i << " * " << i << " = " << i * i << std::endl; }
+
+
+
+	
+
+
+	
+	
+	 
+	
+	return 0;
+
+}
